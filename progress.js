ba11da4f097aba22348e6fe27d93a3e9667710df
Eric <eye1985@users.noreply.github.com>
2017-02-16 20:13:33 +0100

    Add files via upload
    - Double export fixed.
    - Updated README.

---
+++ progress.js
@@ -0,0 +1,23 @@
+import React,{Component} from 'react';
+import {connect} from 'react-redux';
+
+class ProgressBar extends Component {
+    // let progressBar = (isFetching)?<ProgressBar />:undefined;
+    render() {
+        const {isFetching,isFailed,isComplete} = this.props.fetchStatus;
+        const classStr = (isComplete)?'progress no-margin alwaysOnTop hide':'progress no-margin alwaysOnTop';
+
+        return(
+            <div className={classStr}>
+                <div className="indeterminate"></div>
+            </div>
+        )
+    }
+}
+
+
+function mapStateToProps(state){
+    return state;
+}
+
+export default connect(mapStateToProps)(ProgressBar);
